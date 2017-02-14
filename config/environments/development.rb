1ed92099310393078f93e1f9bd199ca248a37f53
baptiste mulliez <baptistem59@hotmail.com>
2017-02-14 18:08:27 +0100

    no babelify command

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,5 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_cable.url = 'ws://localhost:3000/cable'
   config.action_cable.allowed_request_origins = [ /.*/ ]
+  config.browserify_rails.commandline_options = '-t babelify'
 end
