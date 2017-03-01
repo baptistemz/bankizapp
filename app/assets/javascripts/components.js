12a6e75dc85acc754e512d049f864fa89a7abb7b
baptiste mulliez <baptistem59@hotmail.com>
2017-03-02 00:35:45 +0100

    Root not defined in production 3

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/containers/Root.js';
 
-console.log(Root)
 global.React = React
 global.ReactDOM = ReactDOM
 global.Root = Root
